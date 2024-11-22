a2b61692b54066afc70f2cda61906f1be7962d5b
Dumuthu Jayasinghe <126330563+Dumuthu2000@users.noreply.github.com>
2024-11-22 14:06:48 +0530

    Add express-validator

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,7 @@ import express from 'express';
 import { loginUser, registerUser } from '../controllers/userController.js';
 const router = express.Router();
 
-
-router.post('/register',registerUser);
-router.post('/login',loginUser);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 export default router;
